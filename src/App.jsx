c74fb592d755165a6ed9f6b3d2dfed8092f883b8
Mansi26-tech <mansiyadav264.my@gmail.com>
2024-08-12 17:44:53 +0530

    uploaded files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Naviagtion from './components/Naviagtion'
+import Content from './components/content'
+
+export default function App() {
+  return (
+    <div>
+    <Naviagtion/>
+    <Content/>
+    </div>
+  )
+}
